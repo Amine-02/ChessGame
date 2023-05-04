494d8f4ef0aaf81f02c183291ff48a746c221e3c
Amine Mehmel <aminemehmel@gmail.com>
2023-05-04 08:22:40 -0400

    Add case test to verify ChessPiece movements

--- Test.cpp
+++ Test.cpp
@@ -1,12 +1,16 @@
 #include "gtest/gtest.h"
 #include "ChessPiece.h"
-#include "Pawn.h"
+#include "Rook.h"
+#include "Knight.h"
+#include "Bishop.h"
+#include "Queen.h"
+#include "King.h"
 #include "ChessBoard.h"
 
 // Test cases go here
 
 TEST(PawnTest, PawnMovement) {
-	int colorStyle = 0; // Choose a color style
+	int colorStyle = 0;
 	ChessBoard chessBoard(colorStyle);
 	Pawn whitePawn(Team::White, chessBoard);
 	Pawn blackPawn(Team::Black, chessBoard);
@@ -34,3 +38,64 @@ TEST(PawnTest, PawnMovement) {
 	ASSERT_FALSE(blackPawn.isValidMove(from, invalidMove));
 }
 
+TEST(RookTest, RookMovement) {
+	Rook whiteRook(Team::White);
+	Rook blackRook(Team::Black);
+
+	Position from(3, 3);
+	Position validMove(3, 7);
+	Position invalidMove(4, 5);
+
+	ASSERT_TRUE(whiteRook.isValidMove(from, validMove));
+	ASSERT_FALSE(whiteRook.isValidMove(from, invalidMove));
+}
+
+TEST(KnightTest, KnightMovement) {
+	Knight whiteKnight(Team::White);
+	Knight blackKnight(Team::Black);
+
+	Position from(3, 3);
+	Position validMove(5, 4);
+	Position invalidMove(3, 5);
+
+	ASSERT_TRUE(whiteKnight.isValidMove(from, validMove));
+	ASSERT_FALSE(whiteKnight.isValidMove(from, invalidMove));
+}
+
+TEST(BishopTest, BishopMovement) {
+	Bishop whiteBishop(Team::White);
+	Bishop blackBishop(Team::Black);
+
+	Position from(3, 3);
+	Position validMove(5, 5);
+	Position invalidMove(3, 5);
+
+	ASSERT_TRUE(whiteBishop.isValidMove(from, validMove));
+	ASSERT_FALSE(whiteBishop.isValidMove(from, invalidMove));
+}
+
+TEST(QueenTest, QueenMovement) {
+	Queen whiteQueen(Team::White);
+	Queen blackQueen(Team::Black);
+
+	Position from(3, 3);
+	Position validMove1(5, 5);
+	Position validMove2(3, 7);
+	Position invalidMove(4, 6);
+
+	ASSERT_TRUE(whiteQueen.isValidMove(from, validMove1));
+	ASSERT_TRUE(whiteQueen.isValidMove(from, validMove2));
+	ASSERT_FALSE(whiteQueen.isValidMove(from, invalidMove));
+}
+
+TEST(KingTest, KingMovement) {
+	King whiteKing(Team::White);
+	King blackKing(Team::Black);
+
+	Position from(3, 3);
+	Position validMove(4, 4);
+	Position invalidMove(5, 5);
+
+	ASSERT_TRUE(whiteKing.isValidMove(from, validMove));
+	ASSERT_FALSE(whiteKing.isValidMove(from, invalidMove));
+}
